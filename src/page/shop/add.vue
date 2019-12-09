6f08dbf04b368a5af609a67a3339a2db4c5e1445
Kira-li <374228740@qq.com>
2019-12-09 22:08:19 +0800

    change1

--- src/page/shop/add.vue
+++ src/page/shop/add.vue
@@ -5,7 +5,7 @@
       <el-tab-pane label="配置管理" name="second">配置管理</el-tab-pane>
       <el-tab-pane label="角色管理" name="third">角色管理</el-tab-pane>
       <el-tab-pane label="定时任务补偿" name="fourth">定时任务补偿</el-tab-pane>
-  </el-tabs>  
+  </el-tabs>
 </div>
 </template>
 <script>
