ebb162e0270740ceb0dc6214cf2109abf4e6ff9b
Kira-li <374228740@qq.com>
2019-12-12 22:20:41 +0800

    recharge

--- src/page/account/recharge.vue
+++ src/page/account/recharge.vue
@@ -1,27 +1,129 @@
 <template>
 <div class="container-box">
-  <el-tabs v-model="activeName" @tab-click="handleClick">
-      <el-tab-pane label="用户管理" name="first">用户管理</el-tab-pane>
-      <el-tab-pane label="配置管理" name="second">配置管理</el-tab-pane>
-      <el-tab-pane label="角色管理" name="third">角色管理</el-tab-pane>
-      <el-tab-pane label="定时任务补偿" name="fourth">定时任务补偿</el-tab-pane>
-  </el-tabs>
+  <el-row>
+    <el-col :span="12">
+      <div class="accout_detail"><div>当前账户余额(元)：<strong>{{account.canUse}}</strong></div></div>
+      <el-card>
+        <div class="card_mid border_rad">
+          <div><strong>第一步：请先转账到以下账户</strong></div>
+          <p>银行卡号：{{bank.number}}</p>
+          <p>户名：{{bank.name}}</p>
+          <p>开户行：{{bank.bankName}}</p>
+          <p>支行：{{bank.branch}}</p>
+        </div>
+      </el-card>
+    </el-col>
+    <el-col :span="12">
+      <div class="accout_detail"><div>当前冻结余额(元)：<strong>{{account.freeze}}</strong></div></div>
+      <el-card>
+        <div class="card_mid">
+          <div><strong>第二步：确认转账金额</strong></div>
+          <el-form :model="formItem"  label-width="90px">
+            <el-form-item label="转账金额">
+              <el-input v-model="formItem.money" placeholder="转账金额"></el-input>
+            </el-form-item>
+            <el-form-item label="转账银行">
+              <el-select v-model="formItem.bank" placeholder="转账银行">
+                <el-option label="中国银行" value="BOC"></el-option>
+                <el-option label="中国工商银行" value="ICBC"></el-option>
+              </el-select>
+            </el-form-item>
+            <el-form-item label="银行卡姓名">
+              <el-input v-model="formItem.name" :disabled="true" placeholder="银行卡姓名"></el-input>
+            </el-form-item>
+            <el-form-item>
+              <el-button type="primary" size="mini" @click="onCharge" style="margin-top:10px">确定充值</el-button>
+            </el-form-item>
+          </el-form>
+        </div>
+      </el-card>
+    </el-col>
+  </el-row>
+  <div>
+    注意事项：<br>
+    审核时间： 9:00 - 20:00
+  </div>
+  <el-card>
+    <div slot="header" class="clearfix">
+      <span>充值记录</span>
+    </div>
+     <el-table :data="tableData" border style="width: 100%">
+      <el-table-column prop="submissionTime" label="提交时间"></el-table-column>
+      <el-table-column prop="bank" label="转账银行"></el-table-column>
+      <el-table-column prop="branch" label="开户行"></el-table-column>
+      <el-table-column prop="money" label="金额"></el-table-column>
+      <el-table-column prop="examineTime" label="审核时间" ></el-table-column>
+      <el-table-column prop="examineStatus" label="审核状态"></el-table-column>
+      <el-table-column prop="example" label="备注"></el-table-column>
+    </el-table>
+  <el-pagination
+    background
+    layout="prev, pager, next"
+    :page-size="tablePage.pageSize"
+    :current-page="tablePage.page"
+    :total="tablePage.total">
+  </el-pagination>
+  </el-card>
 </div>
 </template>
 <script>
 export default {
   data () {
     return {
-        activeName: 'second'
+        activeName: 'second',
+        account: {
+          canUse: 0,
+          freeze: 0
+        },
+        bank: {
+          number: 222222,
+          name: "lisi",
+          bankName: "中国银行",
+          branch: "杭州支行"
+        },
+        formItem: {
+          money: 33,
+          bank: "BOC",
+          name: "lisi"
+        },
+        tableData: [],
+        tablePage: {
+          page: 2,
+          pageSize: 10,
+          total: 100
+        }
     };
   },
   methods: {
-    handleClick (tab, event) {
-        console.log(tab, event);
+    onCharge () {
+      console.log(11);
     }
   }
 };
 </script>
 
-<style lang="css">
+<style lang="scss" scoped>
+.accout_detail {
+  width: 100%;
+  height: 40px;
+  line-height: 40px;
+  background: #d8c4c0;
+  div {
+    display: table;
+    margin: 0 auto;
+  }
+  strong {
+    color: #ff4000;
+  }
+}
+.card_mid {
+  height: 210px;
+  p,div {
+    margin: 10px auto;
+    width: 200px;
+  }
+}
+.border_rad {
+  border-radius: 5px;
+}
 </style>
